29b164c496c3a536f2a4c4eb6f22a12dde187e79
Abinash <abhi.vj69@gmail.com>
2020-07-08 03:10:35 +0530

    Covid App

---
+++ covidhome.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+from urllib.request import urlopen
+import json
+
+app = Flask(__name__)
+app.secret_key = 'Abhi70#@'
+
+
+@app.route('/')
+def index():
+	with urlopen('https://api.covid19api.com/summary') as res:
+		# data = res.read()
+		data = json.load(res)
+
+
+	for key in data:
+		value = data[key]
+		# for key in value:
+		# 	values = value[key]
+		# 	return key
+		# return value
+	# return data
+	return render_template('index.html', result=data)
+
+
+
+if __name__ == "__main__":
+	app.run()
